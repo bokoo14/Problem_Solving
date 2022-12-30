8b2f6a1233df614b64345c7188dc8a464d82b67e
bokyung <qkrqhru@naver.com>
2022-12-31 04:29:10 +0900

    20221231

---
+++ 11725 트리의 부모 찾기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+tree = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+
+print(tree)
