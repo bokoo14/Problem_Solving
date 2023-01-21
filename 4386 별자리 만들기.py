766212a69da7dd608783b58fd2712acbbf9eb648
보경 <97583162+bokoo14@users.noreply.github.com>
2023-01-21 23:24:15 +0900

    20230121

--- 4386 별자리 만들기.py
+++ 4386 별자리 만들기.py
@@ -6,30 +6,31 @@ import heapq
 n = int(input()) # 별의 개수
 stars = [list(map(float, input().split())) for _ in range(n)]
 
+# 모든 별들 사이의 거리
 graph = [[] for _ in range(n)]
 for i in range(n):
-    x1, y1 = stars[i]
+    x1, y1 = stars[i] 
     for j in range(n):
         if i==j:
             continue
         x2, y2 = stars[j]
         length = ((x2-x1)**2+(y2-y1)**2)**0.5
-        graph[i].append([j, length])
+        graph[i].append([j, length]) # 연결된 별, 길이
 
-visited=[False]*(n)
-heap=[[0, 0]]
-nodeNum=n
+visited=[False]*(n) # n개의 별
+heap=[[0, 0]] #길이, 별
+starNum=n # 별의 수
 answer=0
-while nodeNum:
+while starNum:
     length, node = heapq.heappop(heap)
     if visited[node]:
         continue
-    nodeNum-=1
+    starNum-=1
     visited[node]=True
     answer+=length
-    for x, y in graph[node]:
-        if visited[x]:
+    for thisStar, thisLength in graph[node]:
+        if visited[thisStar]:
             continue
-        heapq.heappush(heap, [x, y])
+        heapq.heappush(heap, [thisLength, thisStar])
 
 print(answer)
