593face7f06171a6669045bd47f72e5305bd10ff
보경 <97583162+bokoo14@users.noreply.github.com>
2023-01-30 23:44:30 +0900

    20230130

---
+++ 12852 1로 만들기2.py
@@ -0,0 +1,11 @@
+# 2023.01.30
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp=[0]*(n+1) # 연산을 하는 횟수의 최솟값
+answer = 0 
+for i in range(n, 0, -1):
+    if i%3==0:
+        dp[]
+        i%=3
