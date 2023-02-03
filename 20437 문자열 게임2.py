c060bafba0e5d571f456a913a9728ac4cfdee079
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-03 20:23:51 +0900

    20230203

--- 20437 문자열 게임2.py
+++ 20437 문자열 게임2.py
@@ -5,10 +5,8 @@ from collections import defaultdict
 
 t = int(input())
 for _ in range(t):
-    w = list(input().rstrip())
+    w = input().rstrip()
     k = int(input())
-    answer1=10**9
-    answer2=0
     dic = defaultdict(list) 
     for i in range(len(w)): # 문자열의 index
         if w.count(w[i])>=k: # 해당하는 index의 문자열의 개수가 k보다 크다면
@@ -17,6 +15,8 @@ for _ in range(t):
     if not dic: # 딕셔너리에 아무 값도 없으면 답이 없음
         print(-1)
     else:
+        answer1=10**9
+        answer2=0
         for i in dic: # 알파벳과 index를 저장한 dic
             for j in range(0, len(dic[i])-k+1): # 원하는 문자가 k개 이상 포함되어야 함->마지막 index: len(dic[i])-k
                 answer1=min(answer1, dic[i][j+k-1]-dic[i][j]+1)
