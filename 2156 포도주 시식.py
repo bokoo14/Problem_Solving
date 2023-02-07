425563c17cd61a31a3ec799d6f6f5a23d9951ee7
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-07 23:40:13 +0900

    20230207

--- 2156 포도주 시식.py
+++ 2156 포도주 시식.py
@@ -6,8 +6,12 @@ input = sys.stdin.readline
 n = int(input())
 grape = [int(input()) for _ in range(n)]
 dp=[0]*(n)
-dp[0], dp[1] = grape[0], grape[0]+grape[1]
-for i in range(2, n):
-    dp[i]=max(dp[i-2], dp[i-1])+grape[i]
-
-print(dp[n-1])
+dp[0] = grape[0]
+if n==2:
+    dp[1]=grape[0]+grape[1]
+if n>2:
+    dp[1] = grape[0]+grape[1]
+    dp[2] = max(grape[0]+grape[1], grape[0]+grape[2], grape[1]+grape[2])
+    for i in range(3, n):
+        dp[i]=max(dp[i-1], dp[i-2]+grape[i], dp[i-3]+grape[i-1]+grape[i])
+print(max(dp))
