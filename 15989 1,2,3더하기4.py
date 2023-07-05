b915f203e78e0b125accacdb915108fc643ebd40
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-05 16:59:52 +0900

    20230704

--- 15989 1,2,3더하기4.py
+++ 15989 1,2,3더하기4.py
@@ -1,21 +1,21 @@
-#15989 123더히기4
-#2022.08.19
+# 15989 123더히기4
+# 2022.08.19
 import sys
+
 input = sys.stdin.readline
 
-T= int(input())
+T = int(input())
 
 for i in range(T):
-    n=int(input())
-    dp=[0]*(n+3)
-    dp[1]=1
-    dp[2]=2
-    dp[3]=3
-    for j in range(4, n+3):
-        dp[j]=dp[j-1]+(dp[j-2]-dp[j-3])
+    n = int(input())
+    dp = [0] * (n + 3)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 3
+    for j in range(4, n + 3):
+        dp[j] = dp[j - 1] + (dp[j - 2] - dp[j - 3])
 
-        if j%3==0:
-            dp[j]+=1
+        if j % 3 == 0:
+            dp[j] += 1
 
     print(dp[n])
-
---
+++ 15990 123더하기.py
@@ -0,0 +1,7 @@
+# 2023.07.04
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+numbers = [int(input()) for _ in range(n)]
