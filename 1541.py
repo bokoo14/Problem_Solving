c76b7450a5aa64a2b8a23ece5f59e19d2f3e04c5
보경 <97583162+bokoo14@users.noreply.github.com>
2023-09-20 23:42:19 +0900

    Update 1541.py

--- 1541.py
+++ 1541.py
@@ -1,22 +1,24 @@
-#1541
-#2022.07.10
-'''
+# 1541
+# 2022.07.10
+"""
 수학, 문자열, 파싱, 그리디 알고리즘
-'''
+"""
 
 import sys
 
-line=sys.stdin.readline().strip().split('-')
+exp = input().split("-")  #'-'를 기준으로 split해서 exp 리스트에 저장
 
-for i in range(len(line)):
-    s=line[i].split('+')
-    ss=0
-    for k in range(len(s)):
-        ss+=int(s[k])
-    line[i]=ss
+num = []  #'-'로 나눈 항들의 합을 저장할 리스트
 
+for i in exp:
+    sum = 0
+    tmp = i.split("+")  # 덧셈을 하기 위해서 '+'를 기준으로 split
+    for j in tmp:  # split한 리스트의 각 요소들을 더해줌
+        sum += int(j)
+    num.append(sum)  # 각 항의 연산 결과(덧셈)를 num에 저장
 
-for i in range(1,len(line)):
-    line[0]-=line[i]
+n = num[0]  # 식의 제일 처음은 숫자로 시작하기 때문에 0번째 값에서 나머지 값들을 빼준다
 
-print(line[0])
+for i in range(1, len(num)):  # 1번째 값부터 n에서 빼준다
+    n -= num[i]
+print(n)
